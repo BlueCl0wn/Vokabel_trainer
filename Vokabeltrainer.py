98475aa4cf9b6f6c435a3a0357b666c418d522d5
BlueCl0wn <45670653+BlueCl0wn@users.noreply.github.com>
2019-02-21 15:05:39 +0100

    Add files via upload

--- Vokabeltrainer.py
+++ Vokabeltrainer.py
@@ -1,9 +1,15 @@
 import random
 
-dictionarys = open("C:/Users/Darek/Python_Scripts/Vokabel Programm/dictionarys.txt", "r")
-print(dictionarys)
-URI = dictionarys[input("Name of the dictionary you want to work with.")]
-eintraege = open(URI)
+class Dicts():
+    def __init__(self, id, name, URI, sprache1, sprache2):
+        self.id = id
+        self.name = name
+        self.URI = URI
+        self.sprache1 = sprache1
+        self.sprache2 = sprache2
+    def toDicts(self):
+        return str(self.id) + " - " + self.name
+
 
 class Entry:
     def __init__(self, deutsch, englisch):
@@ -12,6 +18,22 @@ class Entry:
     def toString(self):
         return self.deutsch + " - " + self.englisch
 
+dictionarys = [Dicts(1, "Deutsch-Englisch", "C:/Users/Darek/Python_Scripts/Vokabel_Programm/Dictionarys/Deutsch-Englisch.txt", "Deutsch", "Englisch"),
+               Dicts(2, "Deutsch-Latein", "C:/Users/Darek/Python_Scripts/Vokabel_Programm/Dictionarys/Deutsch-Latein.txt", "Deutsch", "Latein")]
+eintraege = []
+#-------------------------------------------------------------------------------
+def URI():
+    for i in range(len(dictionarys)):
+        if input == dictionarys[i].id:
+#           i = i
+            return dictionarys[i].URI
+def dictausgabe():
+    for dictionary in dictionarys:
+        print(dictionary.toDicts())
+def openURI(URI):
+    eintraege.append(open(URI)[3:])
+
+#-------------------------------------------------------------------------------
 def eingabe():
     while True:
         deutsch = input("deutsches Wort: ")
@@ -24,6 +46,7 @@ def eingabe():
 
 def abfrage():
     while True:
+        # Wie rum sollen die Vokabel abgefragt werden (Deutsch->Englisch, Englisch->Deutsch, Deutsch<->Englisch)
         abfrageart = input("Wie willst du abgefragt werden? Tippe für Deutsch -> Englisch = 1, für Englisch -> Deutsch = 2, für Deutsch <-> Englisch = 3. ")
         if abfrageart == "#fertig":
             print("-----------------------------------------------------------------------------")
@@ -33,11 +56,16 @@ def abfrage():
             abfrageint <= 3
         except:
             print("Nur die drei angegebenen Möglichkeiten. (1, 2, 3)")
+
+        # 1. Art des Abfragens der Vokabeln (Deutsch->Englisch).
         if abfrageint == 1:
             while True:
                 position = random.randint(1, len(eintraege) - 1)
+                # Es wird überprüft ob die gewählte Vokabel bei den letzetn 2 mal Abfragen schon vorkam und wenn ja wird die Vokabel geändert.
                 if uebersetung == letzte:
                     uebersetzung = eintraege[position + 1]
+                if uebersetzung == vorletzte:
+                    uebersetzung = eintraege[position + 2]
                 uebersetzung = input("Englische Übersetzung von " + eintraege[position].deutsch + ": ")
                 if uebersetzung == "#fertig":
                     print("-----------------------------------------------------------------------------")
@@ -46,12 +74,17 @@ def abfrage():
                     print("Korrekt.")
                 else:
                     print("Leider Falsch. Korrekt ist: "+ eintrage[position].englisch)
+
                 letzte = uebersetzung
+        # 2. Art des Abfragens der Vokabeln (Englisch->Deutsch).
         if abfrageint == 2:
             while True:
                 position = random.randint(1, len(eintraege) - 1)
+                # Es wird überprüft ob die gewählte Vokabel bei den letzetn 2 mal Abfragen schon vorkam und wenn ja wird die Vokabel geändert.
                 if uebersetung == letzte:
                     uebersetzung = eintraege[position + 1]
+                if uebersetzung == vorletzte:
+                    uebersetzung = eintraege[position + 2]
                 uebersetzung = input("Englische Übersetzung von " + eintraege[position].englisch + ": ")
                 if uebersetzung == "#fertig":
                     print("-----------------------------------------------------------------------------")
@@ -61,11 +94,18 @@ def abfrage():
                 else:
                     print("Leider Falsch. Korrekt ist: "+ eintrage[position].deutsch)
                 letzte = uebersetzung
+                vorletzte = letzte
+
+        # 3. Art des Abfragens der Vokabeln (Deutsch<->Englisch).
         if abfrageint == 3:
             while True:
                 position = random.randint(1, len(eintraege) - 1)
+                # Es wird überprüft ob die gewählte Vokabel bei den letzetn 2 mal Abfragen schon vorkam und wenn ja wird die Vokabel geändert.
                 if uebersetung == letzte:
                     uebersetzung = eintraege[position + 1]
+                if uebersetzung == vorletzte
+                    uebersetzung = eintraege[position + 2]
+                # Es wird zufällig eine Abfragerichtung ausgwählt.
                 richtung = random.randint(0,1)
                 if richtung == 0:
                     uebersetzung = input("Englische Übersetzung von " + eintraege[position].deutsch + ": ")
@@ -86,6 +126,7 @@ def abfrage():
                     else:
                         print("Leider Falsch. Korrekt ist: "+ eintrage[position].deutsch)
                 letzte = uebersetzung
+                vorletzte = letzte
 
 def ausgabe():
     for eintrag in eintraege:
@@ -100,10 +141,14 @@ ausgabe um alle gespeicherten Vokabeln auszugeben
 ----------------------------------------------------------------------------""")
 
 while True:
-    befehl = input("Befehl: ")
-    if befehl == "eingabe":
-        eingabe()
-    if befehl == "abfrage":
-        abfrage()
-    if befehl == "ausgabe":
-        ausgabe()
+    dictausgabe()
+    input = int(input("welches dict? "))
+    eintraege.append(openURI(URI()))
+    while True:
+        befehl = input("Befehl: ")
+        if befehl == "eingabe":
+            eingabe()
+        if befehl == "abfrage":
+            abfrage()
+        if befehl == "ausgabe":
+            ausgabe()
